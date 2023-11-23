bd58350784a8f3f802923fc1e515ba14488bc8a4
proc015 <proc015@gmail.com>
2023-11-23 10:20:25 +0100

    updated redux

--- client/src/reducers/stepSlice.js
+++ client/src/reducers/stepSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 export const stepSlice = createSlice({
   name: "step",
